9d232a6ace0fa0ab8c7b0b4223ff06044d6d054c
EwenQuintin <72618375+EwenQuintin@users.noreply.github.com>
2021-05-20 16:41:01 +0200

    Add files via upload

--- src/NavigPlaneurGUI.java
+++ src/NavigPlaneurGUI.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,54 +20,81 @@ public class NavigPlaneurGUI extends JFrame {
 		});
 	}
 	
+	/**
+	 * 
+	 */
 	public NavigPlaneurGUI() {
 		this.initComponents();
 	}
 	
-	public void initComponents() {
+	/**
+	 * 
+	 */
+	private void initComponents() {
 		setTitle("NavigPlaneur");
 		setSize(800,600);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		//setExtendedState(this.MAXIMIZED_BOTH);
 		
+		//
 		ImageIcon fond = new ImageIcon("data/fond.png");
 		JLabel j = new JLabel(fond);
-		
 		JPanel main = new JPanel();
 		main.add(j);
-	
-		JLabel title = new JLabel("NavigPlaneur");
+		
+		//
+		JLabel title = new JLabel("<html><body><u>NavigPlaneur</u></body></html>");
+		Font f1 = new Font("Sherif",Font.PLAIN, 25);
+		title.setFont(f1);
 		JButton settings = new JButton("Settings");
-		
+		title.setHorizontalAlignment(JLabel.CENTER);
+		settings.setSize(120, 35);
+		Font f2 = new Font("Sherif",Font.PLAIN, 15);
+		settings.setFont(f2);
 		JPanel stateBar = new JPanel();
-		stateBar.setLayout(new GridLayout(1,3,260,5));
-		stateBar.add(title);
-		stateBar.add(settings);
-		
-		JLabel nav = new JLabel("Navigation");		
+		add(title, BorderLayout.NORTH);
+		add(settings);
+
+		//
+		JLabel nav = new JLabel("<html><body><u>Navigation</u></body></html>",JLabel.CENTER);	
 		JRadioButton choice1 = new JRadioButton();
 		JRadioButton choice2 = new JRadioButton();
 		JLabel alt = new JLabel("Altitude actuelle");
 		JPanel menu = new JPanel();
 		JTextField altitude = new JTextField("Entrer altitude");
 		JButton darkTheme = new JButton("Thème sombre");
-		menu.setLayout(new GridLayout(7,1));
+		nav.setFont(f2);
+		alt.setFont(f2);
+		altitude.setFont(f2);
+		darkTheme.setFont(f2);
+		choice1.setHorizontalAlignment(JLabel.CENTER);
+		choice2.setHorizontalAlignment(JLabel.CENTER);
+		alt.setHorizontalAlignment(JLabel.CENTER);
+		altitude.setHorizontalAlignment(JLabel.CENTER);
+		darkTheme.setHorizontalAlignment(JLabel.CENTER);
+		menu.setLayout(new GridLayout(7,1,0,-5));
 		menu.add(nav);
 		menu.add(choice1);
 		menu.add(choice2);
 		menu.add(alt);
 		menu.add(altitude);
 		menu.add(darkTheme);
-		
 		add(menu, BorderLayout.WEST);
 
-		
+		//
 		settings.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent evt) {
 				settings(evt);
 			}
 		});
 		
+		//
+		darkTheme.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt) {
+				darkTheme(evt);
+			}
+		});
+		
+		//
 		main.add(stateBar, BorderLayout.NORTH);
 		main.setAlignmentX(CENTER_ALIGNMENT);
 		main.add(j, BorderLayout.EAST);
@@ -74,8 +102,19 @@ public class NavigPlaneurGUI extends JFrame {
 		
 	}
 	
+	/**
+	 * 
+	 * @param evt
+	 */
 	private void settings(ActionEvent evt) {
 		System.out.println("Settings");
 	}
 
+	/**
+	 * 
+	 * @param evt
+	 */
+	private void darkTheme(ActionEvent evt) {
+		System.out.println("Dark theme");
+	}
 }
